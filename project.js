f9cf2a59e13e8ba031b76111d585fe3486dfd6fe
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2019-02-02 11:41:00 +0000

    1st commit
    moving files from work folder

---
+++ project.js
@@ -0,0 +1,28 @@
+// Menu #topNav links
+
+$('#logo').on('click', function() {
+    $('.menus').addClass('inactive');
+    $('#main').removeClass('inactive').addClass('active');
+    
+});
+
+$('#aboutLink').on('click', function() {
+    $('.menus').addClass('inactive');
+    $('#about').removeClass('inactive').addClass('active');
+    $('.nav-link').removeClass('highlight-link');
+    $(this).toggleClass('highlight-link');
+});
+
+$('#projectsLink').on('click', function() {
+    $('.menus').addClass('inactive');
+    $('#projects').removeClass('inactive').addClass('active');
+    $('.nav-link').removeClass('highlight-link');
+    $(this).toggleClass('highlight-link');
+});
+
+$('#contactMeLink').on('click', function() {
+    $('.menus').addClass('inactive');
+    $('#contactMe').removeClass('inactive').addClass('active');
+    $('.nav-link').removeClass('highlight-link');
+    $(this).toggleClass('highlight-link');
+});
