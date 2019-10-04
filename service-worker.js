37f24a86d16f274098216cc4891c408fb51eda95
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-10-04 19:58:11 +0100

    ver 1.6 projectitem style changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.753bd299c1e6c5c62c5ad26cb4a240d4.js"
+  "/precache-manifest.509f6f049c113d7a57d92b6e6e165eda.js"
 );
 
 self.addEventListener('message', (event) => {
